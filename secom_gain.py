53f7a8601af2e1927d0395844dc0b85945858349
mata <mata.lee@nexr.com>
2021-03-23 17:16:02 +0900

    21.03.23 3rd commit

--- secom_gain.py
+++ secom_gain.py
@@ -44,6 +44,11 @@ def data_loader(data_name, miss_rate):
     miss_data_x[data_m == 0] = np.nan
 
     return data_x, miss_data_x, data_m
+#%%
+data_name = 'uci-secom_complete_cv_90'
+miss_rate = 0.2
+
+data_x, miss_data_x, data_m = data_loader(data_name, miss_rate)
 
 #%%
 # GAIN
@@ -69,7 +74,7 @@ from utils import normalization, renormalization, rounding
 from utils import xavier_init
 from utils import binary_sampler, uniform_sampler, sample_batch_index
 
-
+# data_x = miss_data_x
 gain_parameters = {'miss_rate': 0.2, 'batch_size': 128, 'hint_rate': 0.9, 'alpha': 100, 'iterations': 10000}
 
 def gain(data_x, gain_parameters):
@@ -228,3 +233,121 @@ def gain(data_x, gain_parameters):
     imputed_data = rounding(imputed_data, data_x)
 
     return imputed_data
+#%%
+
+#%%
+# main
+'''Main function for UCI letter and spam datasets.
+'''
+
+# Necessary packages
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import numpy as np
+
+# from data_loader import data_loader
+from gain import gain
+from utils import rmse_loss
+
+# ori_data_x = data_x
+# imputed_data_x = imputed_data
+
+def main(args):
+    '''Main function for UCI letter and spam datasets.
+
+    Args:
+      - data_name: letter or spam
+      - miss_rate: probability of missing components
+      - batch:size: batch size
+      - hint_rate: hint rate
+      - alpha: hyperparameter
+      - iterations: iterations
+
+    Returns:
+      - imputed_data_x: imputed data
+      - rmse: Root Mean Squared Error
+    '''
+
+    data_name = args.data_name
+    miss_rate = args.miss_rate
+
+    gain_parameters = {'batch_size': args.batch_size,
+                       'hint_rate': args.hint_rate,
+                       'alpha': args.alpha,
+                       'iterations': args.iterations}
+
+    # Load data and introduce missingness
+    ori_data_x, miss_data_x, data_m = data_loader(data_name, miss_rate)
+
+    # Impute missing data
+    imputed_data_x = gain(miss_data_x, gain_parameters)
+
+    # Report the RMSE performance
+    rmse = rmse_loss(ori_data_x, imputed_data_x, data_m)
+
+    print()
+    print('RMSE Performance: ' + str(np.round(rmse, 4)))
+
+    return imputed_data_x, rmse
+#%%
+
+
+if __name__ == '__main__':
+    # Inputs for the main function
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--data_name',
+        choices=['letter', 'spam'],
+        default='spam',
+        type=str)
+    parser.add_argument(
+        '--miss_rate',
+        help='missing data probability',
+        default=0.2,
+        type=float)
+    parser.add_argument(
+        '--batch_size',
+        help='the number of samples in mini-batch',
+        default=128,
+        type=int)
+    parser.add_argument(
+        '--hint_rate',
+        help='hint probability',
+        default=0.9,
+        type=float)
+    parser.add_argument(
+        '--alpha',
+        help='hyperparameter',
+        default=100,
+        type=float)
+    parser.add_argument(
+        '--iterations',
+        help='number of training iterations',
+        default=10000,
+        type=int)
+
+    args = parser.parse_args()
+
+    # Calls main function
+    imputed_data, rmse = main(args)
+#%%
+# Mean imputation
+from sklearn.impute import SimpleImputer
+
+med_imputer = SimpleImputer(missing_values = np.nan, strategy = 'median')
+med_imputer = med_imputer.fit(miss_data_x)
+imputed_data_med = med_imputer.transform(miss_data_x)
+
+# Report the RMSE performance
+rmse_med = rmse_loss(ori_data_x, imputed_data_med, data_m)
+
+print()
+print('RMSE Performance: ' + str(np.round(rmse_med, 4)))
+#%%
+# EM imputation
+import impyute as impy
+
+imputed_data_em = impy.em(miss_data_x)
